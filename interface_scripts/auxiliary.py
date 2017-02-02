88c83aad99f319c91087495db82f91cca111af2c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-02-02 14:19:18 +0100

    udpated ncl warning message

--- interface_scripts/auxiliary.py
+++ interface_scripts/auxiliary.py
@@ -58,7 +58,7 @@ def print_header(projdict, opt):
         @param project_info dictionary with the necessary information
         @param opt logical for shorter header for the reformat case
     """
-    
+
     vv = 1
     line1 = 54 * "_"
     line2 = 61 * "_"
@@ -103,9 +103,9 @@ def ncl_version_check():
         error("NCL not found")
 
     if out[1] == "6.3.0":
-        info("You are using 6.3.0 which is not supported due to a bug!", 1, 1)
-        #error("NCL version " + out[1] + 
-        #      " not supported due to a bug " + 
+        info("WARNING: You are using NCL version 6.3.0 which is not supported due to a bug!", 1, 1)
+        #error("NCL version " + out[1] +
+        #      " not supported due to a bug " +
         #      "(see Known Issues in the ESMValTool user guide)")
 
     if int(out[1].split(".")[0]) < 6:
